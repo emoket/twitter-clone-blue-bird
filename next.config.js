dfd91c1fbb4770b22cb1a0fb0f419f64324020e7
박용진 <insight.emoket@gmail.com>
2023-08-07 00:33:30 +0900

    github avatar image 사용을 위해 remotePatterns 속성 추가

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
   experimental: {
     serverActions: true,
   },
