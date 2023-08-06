0e08a5e3edda545b3ea683eb6a0f59560d6f7aee
박용진 <insight.emoket@gmail.com>
2023-08-06 11:23:25 +0900

    인증되지 않은 사용자는 로그인 페이지로, 인증된 사용자는 랜딩페이지로 분기 처리 로직 추가

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -5,14 +5,11 @@ import AuthButtonClient from '../auth-button-client';
 
 export default async function Login() {
   const supabase = createServerComponentClient({ cookies });
-
   const {
     data: { session },
   } = await supabase.auth.getSession();
 
-  if (session) {
-    redirect('/');
-  }
+  if (session) redirect('/');
 
   return <AuthButtonClient session={session} />;
 }
--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,9 @@ import AuthButtonServer from './auth-button-server';
 import { redirect } from 'next/navigation';
 
 export default async function Home() {
-  const supabase = createServerComponentClient({ cookies });
+  // 인증되지 않은 사용자를 로그인 페이지로 리디렉션 시키기 위해 트윗 목록을 받아 오기 전에 리디렉션 로직을 추가합니다.
+  // 반대로 로그인 페이지(./login/page.tsx)에서는 이미 인증되어 있다면(session exist) 랜딩페이지('/')로 리디렉션 시킵니다.
+  const supabase = createServerComponentClient<Database>({ cookies });
   const {
     data: { session },
   } = await supabase.auth.getSession();
