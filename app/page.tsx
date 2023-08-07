6dfcdb9c2bc8247969714b01589ae5d0c403df97
박용진 <insight.emoket@gmail.com>
2023-08-07 12:44:09 +0900

    cookies 사용을 위해 encoding 설치 및 force-dynamic 키워드 추가

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import AuthButtonServer from './auth-button-server';
 import NewTweet from './new-tweet';
 import Tweets from './tweets';
 
+export const dynamic = 'force-dynamic';
+
 export default async function Home() {
   // 인증되지 않은 사용자를 로그인 페이지로 리디렉션 시키기 위해 트윗 목록을 받아 오기 전에 리디렉션 로직을 추가합니다.
   // 반대로 로그인 페이지(./login/page.tsx)에서는 이미 인증되어 있다면(session exist) 랜딩페이지('/')로 리디렉션 시킵니다.
